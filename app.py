467b2a88129b57dd68fa788c0fef66fdc023e9a1
sjpbailey <sjpbailey1961@gmail.com>
2020-11-19 23:56:32 -0800

    “Commits”

---
+++ app.py
@@ -0,0 +1,89 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+from flask_jwt_extended  import JWTManager
+
+from db import db
+from blacklist import BLACKLIST
+from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'    # os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_TRACK_MODIFACATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+app.secret_key = 'steve' # app.config['JWT_SECERT_KEY]
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWTManager(app) 
+
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1: #instead of hard coding, you should read from a config file or db
+        return {'is_admin': True}
+    return {'is_admin': False}
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['jti'] in BLACKLIST     
+
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'description': 'The token has expired',
+        'error': 'token_expired'
+    }), 401
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'description': 'Signature verifacation failed.',
+        'error': 'invalid_token'
+    }), 401
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token.',
+        'error': 'authorization_required'
+    }), 401
+
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return jsonify({
+        'description': 'The token is not fresh.',
+        'error': 'fresh_token_required'
+    }), 401
+
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({
+        'description': 'The token has been revoked.',
+        'error': 'token_revoked'
+    }), 401
+
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
+api.add_resource(TokenRefresh, '/refresh')
+
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5000, debug=True)
---
+++ resources/item.py
@@ -0,0 +1,94 @@
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_claims, 
+    jwt_optional, 
+    get_jwt_identity,
+    fresh_jwt_required
+)
+from models.item import ItemModel
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                       type=float,
+                       required=True,
+                       help="This field cannot be left blank!"
+                       )
+    parser.add_argument('store_id',
+                       type=int,
+                       required=True,
+                       help="Every item need a store id."
+                       )
+  
+  
+    @jwt_required
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+        return {'messane': 'Items not found'}, 404
+
+    @fresh_jwt_required
+    def post(self, name): 
+        if ItemModel.find_by_name(name):
+            return{'message': "An item with name '{}' already exisits".format(name)}, 400 
+
+        data = Item.parser.parse_args()    
+
+        item = ItemModel(name, **data) #or ItemModel(name, data['price'], data['store_id']) 
+        
+        try:
+            item.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the item."}, 500     
+
+        return item.json(), 201  
+
+    @jwt_required
+    def delete(self, name):
+        claims = get_jwt_claims()
+        if not claims['is_admin']:
+            return {'message': 'Admin privilege required.'}, 401
+
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+            return {'message': 'Item Deleted'}      
+        return{'message': 'Item not found.'}, 404
+
+
+    #@jwt_required
+    def put(self, name):    
+        data = Item.parser.parse_args()
+        
+        item = ItemModel.find_by_name(name)
+
+        if item is None:
+            item = ItemModel(name, **data) #or ItemModel(name, data['price'], data['store_id'])
+        else:
+            item.price = data['price'], data['store_id']
+        #if item:
+            item.price = data['price']
+        #else:
+        #    item = ItemModel(name, **data)    
+        
+        item.save_to_db() 
+
+        return item.json()
+
+
+class ItemList(Resource):
+    @jwt_optional   
+    def get(self):
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        return {
+            'items': [item['name'] for item in items],
+            'message': 'More data avaliable if you log in.'
+        }, 200
+            
+            # List comprehention
+        #return {'items': list(map(lambda  x: x.json(), ItemModel.query.all())) } # Lambda 'map is used in other languages'
